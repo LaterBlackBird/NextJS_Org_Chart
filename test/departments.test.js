5214c5bdfb268a39ee3e031d39d960fa85fd0d75
Seth Holland <sholland@nexient.com>
2022-05-27 14:47:47 -0500

    resolved error with tr being a decendant of div table row test

--- test/departments.test.js
+++ test/departments.test.js
@@ -7,19 +7,34 @@ describe('Department Row', () => {
   
   beforeEach(() => {
     expectedProps = {
-    "id": 10,
-    "isActive": "true",
-    "name": "Test 1",
-    "dateCreated": "2022-05-18T19:35:54.731Z",
-    "dateUpdated": "2022-05-18T19:42:46.358Z"
+      "id": 10,
+      "isActive": "true",
+      "name": "Test 1",
+      "dateCreated": "2022-05-18T19:35:54.731Z",
+      "dateUpdated": "2022-05-18T19:42:46.358Z"
     }
   });
-
+  
   test('should show the department name on a row', () => {
-    const { getByText } = render(<DepartmentRow department={expectedProps}/>);
-    const deptName = getByText(expectedProps.name);
+    const table = document.createElement('tbody');
+    const { container } = render(<DepartmentRow department={expectedProps}/>, {
+      container: document.body.appendChild(table),
+    })
+    const deptName = screen.getByText(expectedProps.name);
   
     expect(deptName).toBeVisible();
   });
+
+  test('should have edit and delete options', () => {
+    const table = document.createElement('tbody');
+    const { container } = render(<DepartmentRow department={expectedProps}/>, {
+      container: document.body.appendChild(table),
+    })
+    const editLink = screen.getByRole('link', { name: /edit/i});
+    const deleteButton = screen.getByRole('button', { name: /delete/i });
+
+    expect(editLink).toBeVisible();
+    expect(deleteButton).toBeVisible();
+  })
 });
 
